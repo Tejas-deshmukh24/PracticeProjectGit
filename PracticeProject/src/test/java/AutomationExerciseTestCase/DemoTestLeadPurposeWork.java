10d51b3b921cde4e632191227b9ad7b64399ff75
Tejas <Tejas@LAPTOP-8QI28HGD>
2025-06-27 22:44:18 +0530

    add new commment status

--- PracticeProject/src/test/java/AutomationExerciseTestCase/DemoTestLeadPurposeWork.java
+++ PracticeProject/src/test/java/AutomationExerciseTestCase/DemoTestLeadPurposeWork.java
@@ -5,6 +5,10 @@ public class DemoTestLeadPurposeWork {
 	public static void main(String[] args) {
 		
 		System.out.println("Hi welcome");
+		
+		main(args);
+		
+		System.out.println("Hi New Code is Added");
 
 	}
 
