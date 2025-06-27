6e0bf459c4ef0a82a2f83dba60fa53656110ee97
Tejas <Tejas@LAPTOP-8QI28HGD>
2025-06-27 22:21:57 +0530

    BranchXYZ puch

---
+++ PracticeProject/src/test/java/AutomationExerciseTestCase/DemoTestLeadPurposeWork.java
@@ -0,0 +1,11 @@
+package test.java.AutomationExerciseTestCase;
+
+public class DemoTestLeadPurposeWork {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi welcome");
+
+	}
+
+}
